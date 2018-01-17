bfb843b3d7fdcb6c2cdec389f939db0cf7e140f0
Joshua A. Horton <joshua_horton@sil.org>
2018-01-17 16:49:22 +0700

    Minor tweaks post-merge.

--- src/ldml-keyboards.cpp
+++ src/ldml-keyboards.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
-  
+#include "ldmlman.capnp.h"
+#include <capnp/message.h>
+#include <capnp/serialize.h>
+
 int main(int argc, char* argv[]) {
+	::capnp::MallocMessageBuilder message;
+
+	::Trie<Rule>::Builder trie = message.initRoot<Trie<Rule>>();
+
     std::cout << "Hello" << std::endl;
 }
